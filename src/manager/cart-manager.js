3e2aa54678115dda35d7361beaf9992a1c076513
Patricio Pittana <pittanapatricio@gmail.com>
2024-07-21 16:50:35 -0300

    D:/Program Files/Git/api/carts completo

--- src/manager/cart-manager.js
+++ src/manager/cart-manager.js
@@ -9,27 +9,19 @@ class CartManager {
     this.path = path;
   };
 
-  async addCart({ id, products }) {
-    const { prodId, quantity } = products;
+  async addCart() {    
     try {
-
       const arrayCarts = await this.readFile();
-
-      const newCart = {
-        id,
-        products: products || [{
-          prodId,
-          quantity: quantity ? quantity : 1
-        }]
-      };
 
       if (arrayCarts.length > 0) {
         CartManager.lastId = arrayCarts.reduce((maxId, cart) => Math.max(maxId, cart.id), 0);
       }
 
-      newCart.id = ++CartManager.lastId;
+      const newCart = { id: ++CartManager.lastId, products: [] };
+
       arrayCarts.push(newCart);
       this.saveFile(arrayCarts);
+      return newCart;
 
     } catch (error) {
 
@@ -38,6 +30,39 @@ class CartManager {
 
     }
   }
+
+  async getCartById(id) {
+    const carts = await this.readFile();
+    const cartById = carts.find((cart) => cart.id === id);
+    return cartById;
+  }
+
+  async addProductToCart(idCart, idProduct) {
+    const carts = await this.readFile();
+    const cartIndex = carts.findIndex((cart) => cart.id === idCart);
+    const prodIndex = carts[cartIndex].products.findIndex((product) => product.id === idProduct);
+
+    if (cartIndex !== -1) {
+
+      if (prodIndex !== -1) {
+
+        carts[cartIndex].products[prodIndex].quantity++;
+        this.saveFile(carts);
+        return "Product quantity increased";
+
+      } else {
+
+        const newProduct = { id: idProduct, quantity: 1 };
+        carts[cartIndex].products.push(newProduct);
+        this.saveFile(carts);
+        return "Product added to the cart";
+
+      };
+    };
+
+    return "Cart not found";
+
+  };
 
   async readFile() {
     const res = await fs.readFile(this.path, "utf-8");
@@ -48,7 +73,6 @@ class CartManager {
   async saveFile(arrayCart) {
     await fs.writeFile(this.path, JSON.stringify(arrayCart, null, 2));
   }
-
 };
 
 export default CartManager;
